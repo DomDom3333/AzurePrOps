dea1cdc2846c73967b0fedf12998ca4205ee38fd
Dominik Essenhofer <Dominik.essenhofer@gmail.com>
2025-06-23 13:24:48 +0200

    Adds initial Avalonia UI project structure
    Sets up the basic structure for an Avalonia UI application.
    
    This commit creates the necessary files and configurations to
    start developing the AzurePrOps application, including:
    
    - Project files (.csproj, .sln)
    - Application definition (App.axaml, App.axaml.cs)
    - Main window (MainWindow.axaml, MainWindow.axaml.cs)
    - ViewLocator and ViewModelBase classes
    - Assets (icon)
    - .gitignore updates

---
+++ AzurePrOps/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AzurePrOps.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AzurePrOps/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AzurePrOps.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
